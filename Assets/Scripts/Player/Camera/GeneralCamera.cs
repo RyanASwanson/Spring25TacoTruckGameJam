ce6c98eaa7305d18d1ecc57aff23bd0377dcdcc1
Ryan S. <112406526+RyanASwanson@users.noreply.github.com>
2025-02-07 22:09:38 -0600

    CameraMinorUpdate

--- Assets/Scripts/Player/Camera/GeneralCamera.cs
+++ Assets/Scripts/Player/Camera/GeneralCamera.cs
@@ -7,14 +7,14 @@ public class GeneralCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FollowPlayer());
     }
 
     private IEnumerator FollowPlayer()
     {
         while(true)
         {
-
+            transform.position = new Vector3(PlayerMovement.Instance.transform.position.x, transform.position.y, transform.position.z);
         }
     }
 }
